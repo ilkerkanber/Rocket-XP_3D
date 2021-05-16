2a8787ea42c16469f6ec1f11172dcd49940a603c
ilkerkanber <ilkerkanber1903@gmail.com>
2021-05-16 17:15:36 +0300

    StaticWallController
    StaticWallController edited.

--- Assets/GameFolders/Scripts/Concretes/Controller/WallController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controller/WallController.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class WallController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace SampleProject3D.Controller {
 
-    // Update is called once per frame
-    void Update()
+    public class WallController : MonoBehaviour
     {
-        
+        private void OnCollisionEnter(Collision collision)
+        {
+            PlayerController pController = GetComponent<PlayerController>();
+            if (pController != null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    
     }
 }
+
