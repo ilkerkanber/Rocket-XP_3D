e337866065c7a6e1fb9ea36cbbae19f9f1e21e0e
ilkerkanber <ilkerkanber1903@gmail.com>
2021-05-16 19:13:11 +0300

    Controller Edited
    FloorController,WallController Edited.

---
+++ Assets/GameFolders/Scripts/Concretes/Controller/StartFloorController.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+namespace SampleProject3D.Controller
+{
+    public class StartFloorController : MonoBehaviour
+    {
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.collider.GetComponent<PlayerController>() != null)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
