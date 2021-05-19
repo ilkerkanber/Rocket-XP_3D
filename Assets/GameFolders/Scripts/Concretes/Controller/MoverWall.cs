390f58f36e9f80e5905bfd96d3023d4eceba7382
ilkerkanber <ilkerkanber1903@gmail.com>
2021-05-19 21:22:33 +0300

    LevelDesign
    New levels added

--- Assets/GameFolders/Scripts/Concretes/Controller/MoverWall.cs
+++ Assets/GameFolders/Scripts/Concretes/Controller/MoverWall.cs
@@ -8,11 +8,11 @@ namespace SampleProject3D.Controller
     public class MoverWall : WallController
     {
         [SerializeField] Vector3 direction;
-
-        [Range(0f, 1f)]
-        [SerializeField] float factor;
+        [SerializeField] float speed=15f;
 
         Vector3 startPosition;
+        float factor;
+        const float CIRCLE = Mathf.PI * 2f;
 
         void Awake()
         {
@@ -20,6 +20,11 @@ namespace SampleProject3D.Controller
         }
         private void Update()
         {
+            float cycle = Time.time / speed;
+            float sinWave = Mathf.Sin(cycle * CIRCLE);
+
+            factor = (sinWave / 2f) + 0.5f;
+
             Vector3 offset = direction * factor;
             transform.position = offset + startPosition;
         }
